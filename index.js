37f44052dfa5729b956db2794bbd1b2871bf2212
Albert Cao <albertc4@illinois.edu>
2020-04-15 17:51:10 -0500

    finished project

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  const urls = await fetch('https://cfw-takehome.developers.workers.dev/api/variants')
+    .then((response) => {
+      return response.json();
+    });
+  const content = await fetch(urls['variants'][Math.floor(Math.random() * 2)])
+    .then((response) => {
+      return response.text();
+    });
+  return new Response(content, {
+    headers: { 'content-type': 'text/html' },
+  });
 }
